528d16dc7dad6cf077eec87a54e343d6ffaf8aaf
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-09-30 01:42:02 +0600

    event add

--- 18_fragments/learning-fragments/src/App.jsx
+++ 18_fragments/learning-fragments/src/App.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import FoodItems from './components/FoodItems';
 import ErrorMassage from './components/ErrorMassage';
 import './App.css'
+import Container from './components/Container';
 const App = () => {
 
 
@@ -14,11 +15,18 @@ const App = () => {
 
 
   return (
-    <>
-    <h1 className='food-heading'>Healthy Food</h1>
-    <ErrorMassage items={foodItems}></ErrorMassage>
-     <FoodItems items={foodItems}></FoodItems>
-    </>
+<>
+  <Container>
+  <h1 className='food-heading'>Healthy Food</h1>
+  <ErrorMassage items={foodItems}></ErrorMassage>
+   <FoodItems items={foodItems}></FoodItems>
+   </Container>
+   {/* <Container>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A praesentium facere ut fugiat explicabo accusamus odio recusandae dolor, velit harum, porro impedit amet inventore eos voluptas voluptates odit? Ducimus, nam!</p>
+   </Container> */}
+   </>
+ 
+   
   )
 }
 
--- 18_fragments/learning-fragments/src/components/Item.jsx
+++ 18_fragments/learning-fragments/src/components/Item.jsx
@@ -1,11 +1,22 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import styles from './item.module.css'
-const Item = ({foodItems}) => {
-  
+import React from "react";
+import styles from "./item.module.css";
+const Item = ({ foodItems }) => {
+  const handleButtonClick = (event) => {
+    console.log(event);
+    console.log(`${foodItems} being bought`);
+  };
   return (
-  <li className={`${styles['kg-item']}`}><span className={styles["kg-span"]}>{foodItems}</span></li>
-  )
-}
+    <li className={`${styles["kg-item"]} list-group-item`}>
+      <span className={styles["kg-span"]}>{foodItems}</span>
+      <button
+        className={`${styles.button} btn btn-info`}
+        onClick={(event) => handleButtonClick(event)}
+      >
+        Buy
+      </button>
+    </li>
+  );
+};
 
-export default Item
+export default Item;
