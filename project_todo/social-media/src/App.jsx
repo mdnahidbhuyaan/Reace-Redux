b65c5d5f28b4402a15c2c7431bf2e2de21c2dff7
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-10-09 12:32:03 +0600

    sidebar add

--- project_todo/social-media/src/App.jsx
+++ project_todo/social-media/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
     <>
     <div>
     
+      <Sidebar/>
       <Header/>
       <Footer/>
     </div>
