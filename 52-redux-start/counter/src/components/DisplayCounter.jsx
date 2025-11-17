7dcdc9522de0eb91a2eaf174b7232ab6828aa89b
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-11-17 21:32:56 +0600

    dfgdg

---
+++ 52-redux-start/counter/src/components/DisplayCounter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DisplayCounter = () => {
+  return (
+<div className='para'>
+         <p className="lead mb-4 ">
+          Counter current value
+          </p>
+</div>
+  )
+}
+
+export default DisplayCounter
